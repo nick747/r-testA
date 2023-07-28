9bc17a24820d4e81cf0259f7bd18e916099f4181
nick747 <micco.mario@gmail.com>
2023-07-28 17:03:03 +0200

    Created FlutterDemoHomePage app

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+
+function MyButton({ onClick }) {
+  return (
+    <button id='myButton' onClick={onClick}>+</button>
+  );
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [counter, setCounter] = useState(0);
+
+  function incrementCounter() {
+    setCounter((prevCounter) => prevCounter + 1);
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div id='appBar'>
+        <h1>Flutter Demo Home Page</h1>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div id='page'>
+        <label>You have pushed the button this many times:</label>
+        <h1>{counter}</h1>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <MyButton onClick={incrementCounter} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
